f186cd6e4bddb0f8635340d4afa68b41f5fcf8fc
nieznany.sprawiciel <nieznany.sprawiciel@gmail.com>
2023-02-09 14:28:28 +0100

    Add lacking scripts

--- setup/install.sh
+++ setup/install.sh
@@ -2,15 +2,5 @@
 
 set -e
 
-DEFAULT_INSTALL_DIR=~/.local/lib/yagna/plugins
-
-cargo build --release -p ya-runtime-outbound-gateway
-
-mkdir -p ${DEFAULT_INSTALL_DIR}/ya-runtime-outbound-gateway
-
-echo "Installing in ${DEFAULT_INSTALL_DIR}"
-
-cp target/release/ya-runtime-outbound-gateway ${DEFAULT_INSTALL_DIR}/ya-runtime-outbound-gateway/ya-runtime-outbound-gateway
-cp conf/ya-runtime-outbound-gateway.json ${DEFAULT_INSTALL_DIR}/ya-runtime-outbound-gateway
-
-
+./setup/install-runtime.sh
+./setup/provider-setup.sh
