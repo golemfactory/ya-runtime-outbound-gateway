a318cca16806867f998a5470cb973a84a8f9fceb
nieznany.sprawiciel <nieznany.sprawiciel@gmail.com>
2023-02-13 19:01:29 +0100

    Get rid of manifest-support; Usage counters hav unit in name

--- setup/install-runtime.sh
+++ setup/install-runtime.sh
@@ -11,4 +11,4 @@ mkdir -p ${DEFAULT_INSTALL_DIR}/ya-runtime-outbound-gateway
 echo "Installing in ${DEFAULT_INSTALL_DIR}"
 
 cp target/release/ya-runtime-outbound-gateway ${DEFAULT_INSTALL_DIR}/ya-runtime-outbound-gateway/ya-runtime-outbound-gateway
-cp conf/ya-runtime-outbound-gateway.json ${DEFAULT_INSTALL_DIR}/ya-runtime-outbound-gateway
+cp conf/ya-runtime-outbound-gateway.json ${DEFAULT_INSTALL_DIR}/ya-runtime-outbound-gateway.json
