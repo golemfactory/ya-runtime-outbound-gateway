c44fe99bfa8446295ceec4949572443028f6770c
scx1332 <sieciech.czajka@golem.network>
2023-02-21 16:08:46 +0100

    q

--- src/gateway.rs
+++ src/gateway.rs
@@ -75,16 +75,18 @@ impl Runtime for OutboundGatewayRuntime {
 
         // TODO: Here we should start listening on the same protocol as ExeUnit.
         async move {
-            let sock = UdpSocket::bind(format!("127.0.0.1:52001")).await.unwrap();
-            log::info!("Listening on: {}", sock.local_addr().unwrap());
-            let mut buf = [0; 1024];
-            loop {
-                let (len, addr) = sock.recv_from(&mut buf).await.unwrap();
-                log::info!("{len:?} bytes received from {addr:?}");
+            tokio::spawn( async move {
+                let sock = UdpSocket::bind(format!("127.0.0.1:52001")).await.unwrap();
+                log::info!("Listening on: {}", sock.local_addr().unwrap());
+                let mut buf = [0; 1024];
+                loop {
+                    let (len, addr) = sock.recv_from(&mut buf).await.unwrap();
+                    log::info!("{len:?} bytes received from {addr:?}");
 
-                //let len = sock.send_to(&buf[..len], addr).await.unwrap();
-                //ddprintln!("{:?} bytes sent", len);
-            }
+                    //let len = sock.send_to(&buf[..len], addr).await.unwrap();
+                    //ddprintln!("{:?} bytes sent", len);
+                }
+            });
 
             //endpoint.connect(cep).await?;
             Ok(Some(serde_json::json!({
